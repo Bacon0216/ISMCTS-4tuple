b132dbef5edca6bfbac95317e8e4d07203122631
bacon <patrick950216@gmail.com>
2026-04-21 22:27:45 +0800

    bit board Implementation

--- src/4T_header.h
+++ src/4T_header.h
@@ -68,7 +68,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
-
+#include <intrin.h>    // For __popcnt64 intrinsic (Windows)
 // Platform-specific includes for directory handling and timing
 #ifdef _WIN32
 #include <direct.h>
